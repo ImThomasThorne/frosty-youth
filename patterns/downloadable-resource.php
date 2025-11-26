acdfa423c58f4efe487f0d39cfe160549bf78f0f
ImThomasThorne <65457136+ImThomasThorne@users.noreply.github.com>
2025-11-26 15:16:27 +0000

    2.9.0

--- patterns/downloadable-resource.php
+++ patterns/downloadable-resource.php
@@ -6,8 +6,8 @@
  */
 ?>
 
-<!-- wp:group {"metadata":{"name":"Downloadable Resource"},"style":{"spacing":{"padding":{"top":"var:preset|spacing|20","bottom":"var:preset|spacing|20","left":"var:preset|spacing|20","right":"var:preset|spacing|20"},"blockGap":"var:preset|spacing|10"},"elements":{"link":{"color":{"text":"var:preset|color|base"}}},"dimensions":{"minHeight":"20vh"}},"backgroundColor":"primary","textColor":"base","layout":{"type":"flex","orientation":"vertical","verticalAlignment":"space-between","justifyContent":"stretch"}} -->
-<div class="wp-block-group has-base-color has-primary-background-color has-text-color has-background has-link-color" style="min-height:20vh;padding-top:var(--wp--preset--spacing--20);padding-right:var(--wp--preset--spacing--20);padding-bottom:var(--wp--preset--spacing--20);padding-left:var(--wp--preset--spacing--20)"><!-- wp:heading {"level":3,"style":{"elements":{"link":{"color":{"text":"var:preset|color|base"}}},"typography":{"lineHeight":1.6}},"textColor":"base"} -->
+<!-- wp:group {"metadata":{"name":"Downloadable Resource","categories":["cya-blocks"],"patternName":"frost-youth/downloadable-resource"},"style":{"spacing":{"padding":{"top":"var:preset|spacing|20","bottom":"var:preset|spacing|20","left":"var:preset|spacing|20","right":"var:preset|spacing|20"},"blockGap":"var:preset|spacing|10"},"elements":{"link":{"color":{"text":"var:preset|color|base"}}},"dimensions":{"minHeight":"20vh"},"border":{"radius":"8px"}},"backgroundColor":"primary","textColor":"base","layout":{"type":"flex","orientation":"vertical","verticalAlignment":"space-between","justifyContent":"stretch"}} -->
+<div class="wp-block-group has-base-color has-primary-background-color has-text-color has-background has-link-color" style="border-radius:8px;min-height:20vh;padding-top:var(--wp--preset--spacing--20);padding-right:var(--wp--preset--spacing--20);padding-bottom:var(--wp--preset--spacing--20);padding-left:var(--wp--preset--spacing--20)"><!-- wp:heading {"level":3,"style":{"elements":{"link":{"color":{"text":"var:preset|color|base"}}},"typography":{"lineHeight":1.6}},"textColor":"base"} -->
 <h3 class="wp-block-heading has-base-color has-text-color has-link-color" style="line-height:1.6">Downloadable Resource Title</h3>
 <!-- /wp:heading -->
 
@@ -15,7 +15,9 @@
 <p>Short Resource Description</p>
 <!-- /wp:paragraph -->
 
-<!-- wp:file {"id":4646,"href":"https://01901d309eac2ab361f42a15f643d995-16090.sites.k-hosting.co.uk/wp-content/uploads/2025/01/NYA-First-steps-Framework.pdf","displayPreview":false,"align":"wide","className":"resource-download","style":{"elements":{"link":{"color":{"text":"var:preset|color|primary"}}}}} -->
-<div class="wp-block-file alignwide resource-download has-link-color"><a id="wp-block-file--media-a52984d1-2a68-4602-8ca0-e06b00d51205" href="https://01901d309eac2ab361f42a15f643d995-16090.sites.k-hosting.co.uk/wp-content/uploads/2025/01/NYA-First-steps-Framework.pdf">NYA - First steps Framework</a><a href="https://01901d309eac2ab361f42a15f643d995-16090.sites.k-hosting.co.uk/wp-content/uploads/2025/01/NYA-First-steps-Framework.pdf" class="wp-block-file__button wp-element-button" download aria-describedby="wp-block-file--media-a52984d1-2a68-4602-8ca0-e06b00d51205">Download</a></div>
-<!-- /wp:file --></div>
+<!-- wp:file {"align":"wide","className":"resource-download","style":{"elements":{"link":{"color":{"text":"var:preset|color|primary"}}}}} /--></div>
 <!-- /wp:group -->
+
+<!-- wp:paragraph -->
+<p></p>
+<!-- /wp:paragraph -->
--- patterns/linkable-resource.php
+++ patterns/linkable-resource.php
@@ -1,8 +1,23 @@
 <?php
 /**
- * Title: 
- * Slug: 
- * Inserter: 
+ * Title: Linkable Resource
+ * Slug: frost-youth/linkable-resource
+ * Categories:  cya-blocks
  */
 ?>
 
+<!-- wp:group {"metadata":{"name":"Linkable Resource","categories":["cya-blocks"],"patternName":"coblocks_pattern/linkable-resource"},"style":{"spacing":{"padding":{"top":"var:preset|spacing|20","bottom":"var:preset|spacing|20","left":"var:preset|spacing|20","right":"var:preset|spacing|20"},"blockGap":"var:preset|spacing|10"},"elements":{"link":{"color":{"text":"var:preset|color|base"}}},"dimensions":{"minHeight":"20vh"},"border":{"radius":"8px"}},"backgroundColor":"secondary","textColor":"base","layout":{"type":"flex","orientation":"vertical","justifyContent":"stretch","verticalAlignment":"space-between"}} -->
+<div class="wp-block-group has-base-color has-secondary-background-color has-text-color has-background has-link-color" style="border-radius:8px;min-height:20vh;padding-top:var(--wp--preset--spacing--20);padding-right:var(--wp--preset--spacing--20);padding-bottom:var(--wp--preset--spacing--20);padding-left:var(--wp--preset--spacing--20)"><!-- wp:heading {"level":3,"style":{"elements":{"link":{"color":{"text":"var:preset|color|base"}}},"typography":{"lineHeight":1.6}},"textColor":"base"} -->
+<h3 class="wp-block-heading has-base-color has-text-color has-link-color" style="line-height:1.6">Linkable Resource Title</h3>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph -->
+<p>Short Resource Description</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:buttons {"layout":{"type":"flex","justifyContent":"center"}} -->
+<div class="wp-block-buttons"><!-- wp:button {"backgroundColor":"base","textColor":"secondary","width":100,"style":{"elements":{"link":{"color":{"text":"var:preset|color|secondary"}}}},"fontSize":"x-small"} -->
+<div class="wp-block-button has-custom-width wp-block-button__width-100"><a class="wp-block-button__link has-secondary-color has-base-background-color has-text-color has-background has-link-color has-x-small-font-size has-custom-font-size wp-element-button">Link</a></div>
+<!-- /wp:button --></div>
+<!-- /wp:buttons --></div>
+<!-- /wp:group -->
